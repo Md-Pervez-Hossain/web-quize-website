6a0b173f82cd7c6d831b1e11f08f0a9ace821613
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2022-10-12 18:34:06 +0600

    chart responsive

--- src/Cpmponents/Statistics/Statistics.js
+++ src/Cpmponents/Statistics/Statistics.js
@@ -15,21 +15,23 @@ const Statistics = () => {
   const data = useLoaderData().data;
   console.log(data.data);
   return (
-    <div className="md:w-9/12 w-1/2 mx-auto p-3">
-      <div className="flex justify-center mt-28 p-5">
-        <LineChart width={500} height={300} data={data}>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="total"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-          />
-          <Line type="monotone" dataKey="total" stroke="#82ca9d" />
-        </LineChart>
+    <div className="md:w-9/12  mx-auto p-3">
+      <div className="flex justify-center mt-36 p-5">
+        <ResponsiveContainer width="100%" height={400}>
+          <LineChart width={500} height={300} data={data}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="total"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+            <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>
       </div>
     </div>
   );
