eb828f7241e3a74ad9f939718035b77303fa0a81
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2022-10-12 12:05:23 +0600

    statistics section add

--- src/Cpmponents/Statistics/Statistics.js
+++ src/Cpmponents/Statistics/Statistics.js
@@ -1,22 +1,36 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
-// import {
-//   LineChart,
-//   Line,
-//   XAxis,
-//   YAxis,
-//   CartesianGrid,
-//   Tooltip,
-//   Legend,
-//   ResponsiveContainer,
-// } from "recharts";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const Statistics = () => {
   const data = useLoaderData().data;
   console.log(data.data);
   return (
     <div className="md:w-9/12 w-1/2 mx-auto p-3">
-      <div className="flex justify-center mt-28 p-5"></div>
+      <div className="flex justify-center mt-28 p-5">
+        <LineChart width={500} height={300} data={data}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="total"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+        </LineChart>
+      </div>
     </div>
   );
 };
